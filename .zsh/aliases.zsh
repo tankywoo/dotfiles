7c40d538d58b3a8dcee57c895fdfdce0d5c8d5e7
Tanky Woo <wtq1990@gmail.com>
2013-10-18 00:31:31 +0800

    refactor .zshrc

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias tmux="tmux -2"
+alias ls='ls --color=auto'
+alias ll='ls -al'
+alias lls='ls -alSh'
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias grep='grep --color'
+
+alias treepy='tree -I "*.pyc"'
+
+alias vix='vi --cmd "set expandtab"'
+
+# Command-Line Tools `t`
+# https://github.com/tankywoo/t
+alias t='python ~/.t/t.py --task-dir ~/.tasks --list tasks'
+alias tpush='cd ~/.tasks/; git add tasks .tasks.done; git commit -m "add tasks"; git push; cd -'
+alias tpull='cd ~/.tasks/; git pull; cd -'
+alias tst='cd ~/.tasks/; git status; cd -'
