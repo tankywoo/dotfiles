b4e1a23c3cc3557dc04dc0fc587f873ec445a7e4
Tanky Woo <wtq1990@gmail.com>
2015-10-26 10:52:59 +0800

    Make non-tmux with TERM=xterm-256color and tmux with TERM=screen-256color
    Fix tmux with mutt under TERM=xterm-256color not redraw, more detailed
    see
    http://blog.tankywoo.com/2015/10/24/tmux-mutt-not-redraw-problem.html

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,13 +1,18 @@
 # xterm hack for some terminals to support 256 colors
-if [ "$TERM" = "xterm" ]; then
-	if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+
+if [ -z "$TMUX" ] && [[ "$TERM" =~ "xterm" ]]; then
+	if [ -e /usr/share/terminfo/*/xterm-256color ]; then
 		export TERM='xterm-256color'
 	else
 		export TERM='xterm-color'
 	fi
+elif [ -n "$TMUX" ]; then
+	if [ -e /usr/share/terminfo/*/screen-256color ]; then
+		export TERM='screen-256color'
+	else
+		export TERM='screen'
+	fi
 fi
-
-export TERM='xterm-256color'
 export PATH=${PATH}:/sbin:/usr/local/bin:/usr/local/sbin:${HOME}/.local/bin/:
 export TZ='Asia/Shanghai'
 export EDITOR='vim'
