25965d8db07919edef6bb53ef7db24245f19d245
Tanky Woo <wtq1990@gmail.com>
2016-08-10 10:47:48 +0800

    bash functions: set tab name functions

--- .common/functions.sh
+++ .common/functions.sh
@@ -10,3 +10,21 @@ ntpupdate(){ $SUDO ntpdate cn.pool.ntp.org; }
 #ntpupdate(){ $SUDO ntpdate jp.pool.ntp.org; }
 
 i(){ curl ip.cn/$i; }
+
+
+# -------------------------------------------------------------------------------
+# set window/tab title
+# $1 = type; 0 - both, 1 - tab, 2 - title
+# ref:
+#   - Change iTerm2 window and tab titles in zsh
+#   - http://superuser.com/questions/419775/with-bash-iterm2-how-to-name-tabs
+# `echo -ne "\e]1;your title\a"` for simple
+# TODO not work under tmux
+# -------------------------------------------------------------------------------
+setTerminalText() {
+  local mode=$1 ; shift
+  echo -ne "\033]$mode;$@\007"
+}
+set_both() { setTerminalText 0 $@; }
+set_tab() { setTerminalText 1 $@; }
+set_window() { setTerminalText 2 $@; }
