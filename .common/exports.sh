710a2db293d6dd49e529780866367c2a636c434c
Tanky Woo <wtq1990@gmail.com>
2016-12-15 22:44:17 +0800

    Add $GOPATH env

--- .common/exports.sh
+++ .common/exports.sh
@@ -20,7 +20,9 @@ elif [ -n "$TMUX" ]; then
 		export TERM='screen'
 	fi
 fi
-export PATH=${HOME}/.local/bin:/usr/local/bin:/usr/local/sbin:${PATH}
+
+export GOPATH=${HOME}/.go
+export PATH=${HOME}/.local/bin:/usr/local/bin:/usr/local/sbin:$GOPATH/bin:${PATH}
 export TZ='Asia/Shanghai'
 export EDITOR='vim'
 export LANG='en_US.UTF-8'
