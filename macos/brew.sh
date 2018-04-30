c4dcc5cc36a69fc5ec781b187454fb03de5df008
Tanky Woo <wtq1990@gmail.com>
2018-04-30 12:20:51 +0800

    Add macos brew installed packages list

---
+++ macos/brew.sh
@@ -0,0 +1,81 @@
+# Brew
+brew install ack
+brew install aria2
+brew install arping
+brew install autojump
+brew install axel
+brew install bash-completion
+brew install bower
+brew install coreutils
+brew install curl
+brew install dos2unix
+brew install findutils
+brew install flake8
+brew install fping
+brew install fzf
+brew install gawk
+brew install geoip
+brew install gist
+brew install git
+brew install git-cal
+brew install gnu-getopt
+brew install gnu-sed
+brew install go
+brew install gotags
+brew install govendor
+brew install highlight
+brew install htop
+brew install httpie
+brew install ipcalc
+brew install iproute2mac
+brew install jq
+brew install macvim
+brew install mercurial
+brew install mitmproxy
+brew install mongodb
+brew install mosh
+brew install mtr
+brew install mutt
+brew install mycli
+brew install mysql
+brew install nginx
+brew install ngrep
+brew install nmap
+brew install node
+brew install openssh
+brew install openssl
+brew install openssl@1.1
+brew install openvpn
+brew install pandoc
+brew install pdsh
+brew install polipo
+brew install pssh
+brew install pstree
+brew install python
+brew install python3
+brew install python@2
+brew install ranger
+brew install redis
+brew install shadowsocks-libev
+brew install shellcheck
+brew install simple-obfs
+brew install sqlite
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install tree
+brew install unrar
+brew install vim
+brew install watch
+brew install wget
+brew install zsh-completions
+
+
+# Brew Cask
+brew cask install firefox
+brew cask install google-chrome
+brew cask install macdown
+brew cask install osxfuse
+brew cask install vagrant
+brew cask install veracrypt
+brew cask install virtualbox
