21a6164e26b3c7b25f0bb398b6dad05bab94f1df
Tanky Woo <wtq1990@gmail.com>
2025-01-10 15:23:51 +0800

    common: update

--- common/aliases.sh
+++ common/aliases.sh
@@ -1,21 +1,14 @@
-alias tmux="tmux -2"
+# shellcheck shell=bash
 
-if [[ "$(uname)" == "Darwin" ]]; then
-	# For Mac OS
-	alias ls='ls -hG'
-elif [[ "$(uname)" == "Linux" ]];then
-	# For Linux
-	alias ls='ls --color=auto'
-fi
+alias ls='ls --color=auto'
 alias ll='ls -al'
 alias lls='ls -alSh'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-alias grep='grep --color'
 
+alias tmux="tmux -2"
 alias vix='vim --cmd "set expandtab"'
-
 alias treepy='tree -I "*.pyc"'
 alias json="python -mjson.tool"
 alias ccat="pygmentize -f terminal256 -O style=monokai -g"
@@ -24,9 +17,9 @@ alias mux="tmuxinator"
 #@bug: fc -l cause fzf works wrong
 #alias history='fc -l'  # defult: aliased to fc -l 1
 #export HISTTIMEFORMAT='%F %T' # for Bash only
-if [[ "$SHELL" == `which zsh 2>/dev/null` ]]; then
+if [[ "$SHELL" == $(which zsh 2>/dev/null) ]]; then
 HISTFILE=~/.zsh_history
-elif [[ "$SHELL" == `which bash 2>/dev/null` ]]; then
+elif [[ "$SHELL" == $(which bash 2>/dev/null) ]]; then
 HISTFILE=~/.bash_history
 fi
 HISTSIZE=9999
