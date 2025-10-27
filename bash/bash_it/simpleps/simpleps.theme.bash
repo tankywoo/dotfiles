d08596792a2fea99e7fc01d0162e53b7a12ae6d4
Tanky Woo <wtq1990@gmail.com>
2025-10-27 18:46:47 +0800

    增加简化主题快速切换alias

---
+++ bash/bash_it/simpleps/simpleps.theme.bash
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034 # Expected behavior for themes.
+
+# use true color under mosh ssh
+
+function prompt_command() {
+	PS1="${green?}\h ${reset_color?}${blue?}\$${bold_blue?}${reset_color?} ${normal?}"
+}
+
+safe_append_prompt_command prompt_command
--- common/aliases.sh
+++ common/aliases.sh
@@ -13,3 +13,6 @@ alias treepy='tree -I "*.pyc"'
 alias json="python -mjson.tool"
 alias ccat="pygmentize -f terminal256 -O style=monokai -g"
 alias mux="tmuxinator"
+
+alias simpleps="gsed -i \"s|BASH_IT_THEME='candy_modified'|BASH_IT_THEME='simpleps'|g\" $HOME/.dotfiles/bash/bashrc; bash-it reload"
+alias unsimpleps="gsed -i \"s|BASH_IT_THEME='simpleps'|BASH_IT_THEME='candy_modified'|g\" $HOME/.dotfiles/bash/bashrc; bash-it reload"
